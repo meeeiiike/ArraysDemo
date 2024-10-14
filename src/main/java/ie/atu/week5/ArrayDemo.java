5e0225ef6fa1252636dabd5e6c75cc111ac822b7
meeeiiike <g00407095@atu.ie>
2024-10-14 21:44:31 +0100

    Out of Bounds Example
    +
    
    Removed old comments

--- src/main/java/ie/atu/week5/ArrayDemo.java
+++ src/main/java/ie/atu/week5/ArrayDemo.java
@@ -34,13 +34,6 @@ public class ArrayDemo {
             System.out.println(i);
         }
 
-        // Rewrite you application so that the user is prompted for a new value and then try to store this value
-        // in location anArray[10]. Print out the result. Ideally, you should watch what happens using a
-        // breakpoint in the debugger
-
-        // Create a branch called “BugFix1” and rewrite your application so that you can deal with this
-        // exception using a try/catch block. If it works, then push to Master using a pull request.
-
         // Rewrite your application to use an enhanced for loop if you have not done so already. Move this
         // loop to a method so we can reuse the code, if needed. Create this new feature on a branch. If it
         // works after your code change, then push to Master using a pull request
